47b1742f2b55cc623158a7fe6752e9207f48bfd1
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-05-18 01:31:53 +0530

    Add files via upload

---
+++ src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <p>Contact Us :</p>
+        <small>adishi1508@gmail.com</small>{" "}
+      </div>
+      <div className="social">
+        <div>
+          <small>&copy; Copyright 2021</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
